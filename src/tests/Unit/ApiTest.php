13c3e2e50b6b3e260b0957c26c1519b217d44d6b
Raul Garcia Ameyugo <rgarcia@gemweb.es>
2023-11-02 20:26:53 +0100

    Adding api tests: tagParamError Checks if tag param error is working dateFromFormatError checks if dateFrom format error is working dateToFormatError Checks if dateTo format error is working dateFromGtDateTo Checks if dateFrom > dateTo error is working

---
+++ src/tests/Unit/ApiTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Infrastructure\StackExchangeApi;
+
+class ApiTest extends TestCase
+{
+    /**  
+     * @test 
+     * 
+     * Checks if tag param error is working
+     * */
+    public function tagParamError(): void
+    {
+        $response = $this->get('/api/get');
+        $response->assertJson([
+            'error' => true,
+            'errorCode' => 20
+        ]);
+    }
+
+    /**  
+     * @test 
+     * 
+     * Checks if dateFrom format error is working
+     * */
+    public function dateFromFormatError(): void
+    {
+        $response = $this->get('/api/get/test_tag/202s3-01-01');
+        $response->assertJson([
+            'error' => true,
+            'errorCode' => 21
+        ]);
+    }
+
+    /**  
+     * @test 
+     * 
+     * Checks if dateTo format error is working
+     * */
+    public function dateToFormatError(): void
+    {
+        $response = $this->get('/api/get/test_tag/2023-01-01/2023-15-01');
+        $response->assertJson([
+            'error' => true,
+            'errorCode' => 21
+        ]);
+    }
+
+    /**  
+     * @test 
+     * 
+     * Checks if dateFrom > dateTo error is working
+     * */
+    public function dateFromGtDateTo(): void
+    {
+        $response = $this->get('/api/get/test_tag/2023-01-02/2023-01-01');
+        $response->assertJson([
+            'error' => true,
+            'errorCode' => 22
+        ]);
+    }
+
+}
