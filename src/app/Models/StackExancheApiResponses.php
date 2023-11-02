f72a5e331d69a7555e8950ab1d4475c9da69d315
Raul Garcia Ameyugo <rgarcia@gemweb.es>
2023-11-02 21:18:39 +0100

    Databse configuration and StackExchangeApiResponse model creation.

---
+++ src/app/Models/StackExancheApiResponses.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StackExchangeApiResponses extends Model
+{
+    use HasFactory;
+
+    protected $table = 'stackexchange_api_responses';
+}
